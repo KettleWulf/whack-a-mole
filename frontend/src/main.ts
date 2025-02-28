8a6aa35f6a2564b85f3b4f7fb8ed86bf2623d9cf
Oktavian Paunku <oggy134@hotmail.com>
2025-02-28 19:50:32 +0100

    Created a grid system, and test for displaying the mole

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,9 +1,11 @@
 import { io, Socket } from "socket.io-client";
-import {
-	ClientToServerEvents,
-	ServerToClientEvents,
-} from "@shared/types/SocketEvents.types";
+import { ClientToServerEvents, ServerToClientEvents } from "@shared/types/SocketEvents.types";
+import Mole1 from "./assets/images/Mole1.png";
+import Mole2 from "./assets/images/Mole2.png";
+import Mole3 from "./assets/images/Mole3.png";
+import Mole4 from "./assets/images/Mole4.png";
 import "./assets/scss/style.scss";
+
 
 const SOCKET_HOST = import.meta.env.VITE_SOCKET_HOST;
 console.log("🙇 Connecting to Socket.IO Server at:", SOCKET_HOST);
@@ -30,3 +32,40 @@ socket.on("disconnect", () => {
 socket.io.on("reconnect", () => {
 	console.log("😊 Reconnected to server:", socket.io.opts.hostname + ":" + socket.io.opts.port);
 });
+
+const gridContainer = document.querySelector(".grid-container") as HTMLDivElement;
+
+const moleImages = [
+    Mole1,
+    Mole2,
+    Mole3,
+    Mole4
+];
+
+for (let i = 1; i <= 10; i++) {
+    for (let j = 1; j <= 10; j++) {
+        const gridEl = document.createElement("div");
+        gridEl.classList.add("grid-field");
+        gridEl.dataset.coords = `${i}-${j}`;
+        gridEl.textContent = `X:${i} Y:${j}`;
+        gridContainer.appendChild(gridEl);
+    }
+}
+
+const molePositionX = Math.floor(Math.random() * 9) + 1;
+const molePositionY = Math.floor(Math.random() * 9) + 1;
+const moleElement = document.querySelector(`[data-coords="${molePositionX}-${molePositionY}"]`);
+
+if (moleElement instanceof HTMLElement) {
+    const randomMoleImage = moleImages[Math.floor(Math.random() * moleImages.length)];
+	console.log("Slumpmässig mulvad bild:", randomMoleImage);
+
+    const moleDiv = document.createElement("div");
+    moleDiv.classList.add("mole");
+    moleDiv.style.backgroundImage = `url('${randomMoleImage}')`;
+	console.log("Mulvad div lagt till:", moleDiv);
+
+    moleElement.appendChild(moleDiv);
+} else {
+    console.error(`Elementet med position ${molePositionX}-${molePositionY} hittades inte!`);
+}
