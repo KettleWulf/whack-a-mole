0fb0b69879b9fc37c5b1c1fbf9135e8dc3760947
Fredrik Wiking <wikingfredrik@gmail.com>
2025-03-13 18:42:13 +0100

    Forgotten to reset ability to click on virus

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -137,6 +137,7 @@ playerFormEl.addEventListener("submit", (e) => {
 
 
 const startgameCallback = (response: GameDataOmitID) => {
+	canClickvirus = !canClickvirus
 	gridContainer.innerHTML = "";
 	for (let i = 1; i <= 10; i++) {
         for (let j = 1; j <= 10; j++) {
